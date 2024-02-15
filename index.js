805e78a702d6da19699ee5f0489e49518e09a4c5
Herny <hjviltez@gmail.com>
2024-02-15 19:15:58 -0300

    3

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-/*dasd*/ 
+var texto = hola
+console.log(texto);
+
+function encriptar(){
+    var text = document.getElementById("textArea1").value;
+    var textEncript = text
+        .replace(/a/g, "efe")
+        .replace(/e/g, "fefe")
+}
