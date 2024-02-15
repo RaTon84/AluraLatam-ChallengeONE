a42d921dba0c12e619392c8ba2943f8651efbd53
Hernan <110001074+RaTon84@users.noreply.github.com>
2024-02-15 20:22:12 -0300

    5

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 function encriptar() {
     if (document.getElementById("textArea1").value.length == 0) {
         alert("ingrese un texto");      
@@ -22,3 +18,21 @@ function encriptar() {
     }
 };
 
+function desencriptar() {
+    if (document.getElementById("textArea1").value.length == 0) {
+        alert("ingrese un texto");      
+    } else {
+        var text = document.getElementById("textArea1").value;        
+        var textEncript = text
+            .replace(/efe/g, "a")
+            .replace(/few/g, "e")
+            .replace(/dec/g, "i")
+            .replace(/wce/g, "o")
+            .replace(/thn/g, "u")
+            .replace(/r/g, "umt")
+            .replace(/l/g, "olp")
+            .replace(/p/g, "wetf")
+            console-console.log(textEncript);
+        document.getElementById("textArea2").value = textEncript               
+    }
+};
