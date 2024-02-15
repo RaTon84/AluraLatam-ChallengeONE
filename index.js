0cf9c54aa9e5b66b08183241d0abec926833f70c
Herny <hjviltez@gmail.com>
2024-02-15 19:46:46 -0300

    4

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-var texto = hola
-console.log(texto);
 
-function encriptar(){
-    var text = document.getElementById("textArea1").value;
-    var textEncript = text
-        .replace(/a/g, "efe")
-        .replace(/e/g, "fefe")
-}
+
+
+
+
+function encriptar() {
+    if (document.getElementById("textArea1").value.length == 0) {
+        alert("ingrese un texto");      
+    } else {
+        var text = document.getElementById("textArea1").value;        
+        var textEncript = text
+            .replace(/a/g, "efe")
+            .replace(/e/g, "few")
+            .replace(/i/g, "dec")
+            .replace(/o/g, "wce")
+            .replace(/u/g, "thn")
+            .replace(/r/g, "umt")
+            .replace(/l/g, "olp")
+            .replace(/p/g, "wetf")
+            console-console.log(textEncript);
+        document.getElementById("textArea2").value = textEncript               
+    }
+};
+
