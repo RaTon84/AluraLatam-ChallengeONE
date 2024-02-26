8046195a597b7c1daf9e7a3e276b8c359d832d9a
Herny <hjviltez@gmail.com>
2024-02-26 11:08:29 -0300

    mensjaes terminados

--- index.js
+++ index.js
@@ -1,88 +1,121 @@
+function msjIngreseTexto() {
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "No ingreso un texto a encriptar"
+    });
+}
+
+function msjSinTexto() {
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "No hay texto para copiar"
+    });
+}
+
+function msjEncriptar() {
+    Swal.fire({
+        title: "Encriptando Texto",
+        html: "Esta ventana se cerrara en <b></b> milliseconds.",
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+            Swal.showLoading();
+            const timer = Swal.getPopup().querySelector("b");
+            timerInterval = setInterval(() => {
+                timer.textContent = `${Swal.getTimerLeft()}`;
+            }, 100);
+        },
+        willClose: () => {
+            clearInterval(timerInterval);
+        }
+    }).then((result) => {
+        /* Read more about handling dismissals below */
+        if (result.dismiss === Swal.DismissReason.timer) {
+            console.log("I was closed by the timer");
+        }
+    });
+}
+
 function encriptar() {
     if (document.getElementById("textArea1").value.length == 0) {
-        alert("ingrese un texto");      
+        msjIngreseTexto();
     } else {
-        var text = document.getElementById("textArea1").value.toLowerCase();
-        console.log(text)     
-        var textEncript = text
-            .replace(/a/gi, "rsyx")
-            .replace(/e/gi, "dfg")
-            .replace(/i/gi, "nbm")
-            .replace(/o/gi, "lopw")
-            .replace(/u/gi, "iku")            
-            console-console.log(textEncript);
-        document.getElementById("textArea2").value = textEncript;  
-        location.href = "#result";
-        document.getElementById("textArea2").style = "background: none;";           
+        msjEncriptar();
+        setTimeout(function () {
+            let timerInterval;
+            var text = document.getElementById("textArea1").value.toLowerCase();
+            console.log(text)
+            var textEncript = text
+                .replace(/a/gi, "rsyx")
+                .replace(/e/gi, "dfg")
+                .replace(/i/gi, "nbm")
+                .replace(/o/gi, "lopw")
+                .replace(/u/gi, "iku")
+            console - console.log(textEncript);
+            document.getElementById("textArea2").value = textEncript;
+            location.href = "#textArea2";
+            document.getElementById("textArea2").style = "background: none;";
+        }, 1000);
     }
 };
+
 function desencriptar() {
     if (document.getElementById("textArea1").value.length == 0) {
-        alert("ingrese un texto");      
+        msjIngreseTexto();
     } else {
-        var text = document.getElementById("textArea1").value;        
-        var textEncript = text
-            .replace(/rsyx/gi, "a")
-            .replace(/dfg/gi, "e")
-            .replace(/nbm/gi, "i")
-            .replace(/lopw/gi, "o")
-            .replace(/iku/gi, "u")          
-            console-console.log(textEncript);
-        document.getElementById("textArea2").value = textEncript;
-        location.href = "#result";
-        document.getElementById("textArea2").style = "background: none;";              
+        msjEncriptar();
+        setTimeout(function () {
+            var text = document.getElementById("textArea1").value;
+            var textEncript = text
+                .replace(/rsyx/gi, "a")
+                .replace(/dfg/gi, "e")
+                .replace(/nbm/gi, "i")
+                .replace(/lopw/gi, "o")
+                .replace(/iku/gi, "u")
+            console - console.log(textEncript);
+            document.getElementById("textArea2").value = textEncript;
+            location.href = "#textArea2";
+            document.getElementById("textArea2").style = "background: none;";
+        }, 1000);
     }
 };
-function sacarImagen(){
-    if (document.getElementById("textArea2").value.length == 0){
+
+function sacarImagen() {
+    if (document.getElementById("textArea2").value.length == 0) {
         document.getElementById("textArea2").style = "background-image: url(./img/Muñeco.png)";
-    }else
-    document.getElementById("textArea2").style = "background: none;"; 
+    } else
+        document.getElementById("textArea2").style = "background: none;";
 }
-function copiar(){      
-    navigator.clipboard.writeText(document.getElementById("textArea2").value);
-    document.getElementById("textArea2").value = "";
-    document.getElementById("textArea1").value = "";
-    location.href = "#encriptador";
-    document.getElementById("textArea2").style = "background-image: url(./img/Muñeco.png)";
+
+function copiar() {
+    if (document.getElementById("textArea2").value.length == 0) {
+        msjSinTexto();
+    } else {
+        Swal.fire("Mensaje Copiado");
+        navigator.clipboard.writeText(document.getElementById("textArea2").value);
+        document.getElementById("textArea2").value = "";
+        document.getElementById("textArea1").value = "";
+        location.href = "#encriptador";
+        document.getElementById("textArea2").style = "background-image: url(./img/Muñeco.png)";
+    }
 }
-var modo="light";
-function cambiarModo(){    
-    if(modo=="light"){
-        document.querySelector('body').setAttribute("data-bs-theme","dark");        
+
+var modo = "light";
+function cambiarModo() {
+    if (modo == "light") {
+        document.querySelector('body').setAttribute("data-bs-theme", "dark");
         document.querySelector("#iconoModo").setAttribute("class", "bi bi-brightness-high-fill");
         document.querySelector("#iconoModo").setAttribute("style", "color: yellow;");
         document.querySelector("#logo-imagen").setAttribute("src", "./img/logo-alura-white.png")
-        modo="oscuro";
-    }else{
-        document.querySelector('body').setAttribute("data-bs-theme","light");        
+        modo = "oscuro";
+    } else {
+        document.querySelector('body').setAttribute("data-bs-theme", "light");
         document.querySelector("#iconoModo").setAttribute("class", "bi bi-moon-fill ");
         document.querySelector("#iconoModo").setAttribute("style", "color: black;");
         document.querySelector("#logo-imagen").setAttribute("src", "./img/logo-alura-black.png")
-        modo="light";
+        modo = "light";
     }
 }
-/*------------------------------------------------*/
-let timerInterval;
-Swal.fire({
-  title: "Auto close alert!",
-  html: "I will close in <b></b> milliseconds.",
-  timer: 2000,
-  timerProgressBar: true,
-  didOpen: () => {
-    Swal.showLoading();
-    const timer = Swal.getPopup().querySelector("b");
-    timerInterval = setInterval(() => {
-      timer.textContent = `${Swal.getTimerLeft()}`;
-    }, 100);
-  },
-  willClose: () => {
-    clearInterval(timerInterval);
-  }
-}).then((result) => {
-  /* Read more about handling dismissals below */
-  if (result.dismiss === Swal.DismissReason.timer) {
-    console.log("I was closed by the timer");
-  }
-});
-/*---------------------------------------------------- */
+
